67e560c3f880ed7b4b4a3012404b98d6e29f1b5e
An-Junyeop <thwo0075@gmail.com>
2022-02-20 13:54:21 +0900

    Styled components 간단 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Circle = styled.div`
+	width: 10rem;
+	height: 10rem;
+	background: aquamarine;
+	border-radius: 50%;
+	text-align: center;
+	line-height: 10rem;
+	color: ${props => props.color || 'black'};
+
+	${props =>
+		props.huge &&
+		css`
+			width: 20rem;
+			height: 20rem;
+			line-height: 20rem;
+		`}
+`;
 
 function App() {
-	return <div>Styled components learn</div>;
+	return (
+		<Circle color='red' huge>
+			Styled components
+		</Circle>
+	);
 }
 
 export default App;
